c690c0be5b641326563254589349bdeefc0b7eeb
aashishthakurcodes <j9183208@gmail.com>
2023-09-30 18:42:07 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-import express from 'express'
-import connectDb from './Config/dbConnect.js'
-import menuRoutes from './Routes/menuRoutes.js'
-import contactUs from './Routes/contactUs.js'
-import cors from 'cors'
-const app =express()
-connectDb()
+import express from "express";
+import connectDb from "./Config/dbConnect.js";
+import menuRoutes from "./Routes/menuRoutes.js";
+import contactUs from "./Routes/contactUs.js";
+import cors from "cors";
+import path, { dirname } from "path"; // Fixed import statements
+import { fileURLToPath } from "url";
+const app = express();
+connectDb();
 app.use(express.json());
 app.use(cors());
-//Routes
-app.use('/api/v1',menuRoutes)
-app.use('/api/v1',contactUs) 
 
-const PORT =8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+//Routes
+app.use("/api/v1", menuRoutes);
+app.use("/api/v1", contactUs);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// Rest API
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html")); // Fixed path.join
+});
+
+const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on the port=${PORT}`);
-  });
+  console.log(`Server is running on the port=${PORT}`);
+});
